8bd7da2dd7176b55a4d8cbe1773f213ae450cb66
wenwang2 <wenwang@gsd.harvard.edu>
2019-01-07 21:21:44 -0500

    add functions to calculator.js

---
+++ calculator.js
@@ -0,0 +1,18 @@
+// This function adds two numbers 
+function multiply(a,b) { 
+    return a + b
+}
+// This function subtracts two numbers 
+function multiply(a,b) { 
+    return a - b
+}
+
+// This function multiplies two numbers
+function multiply(a,b) { 
+    return a * b
+}
+
+// This function divides two numbers
+function multiply(a,b) { 
+    return a / b
+}
