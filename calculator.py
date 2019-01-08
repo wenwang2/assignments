67b592ab8162ebabf6048581487d0ebded056045
wenwang2 <wenwang@gsd.harvard.edu>
2019-01-07 20:34:12 -0500

    add functions to calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# This function adds two numbers 
+def add(a, b):
+   return a + b
+print(x)
+# This function subtracts two numbers 
+def subtract(a, b):
+   return a - b
+
+# This function multiplies two numbers
+def multiply(a, b):
+   return a * b
+
+# This function divides two numbers
+def divide(a, b):
+   return a / b
